716740c77310fe709edccfa2a61c388e14ee5197
virtuextreme <55068693+virtuextreme@users.noreply.github.com>
2019-12-04 18:04:17 -0500

    additional infomation page

---
+++ compression.php
@@ -0,0 +1,17 @@
+<?php
+    $title = 'Compression';
+    require_once 'includes/header.php';
+    require_once 'db/conn.php';
+    $result = $crud->getSpecialties();
+
+?>
+<p>
+<h1>COMPRESSION HOSIERY</h1>
+<h2>Medical grade compression garments help many patients effectively improve on the venous and lymphatic blood flow of the lower extremities
+ and are used to prevent and treat poor circulation. Compression garments help relieve and 
+ control edema, swelling, varicose veins, leg fatigue and other problematic leg conditions.<h2>
+
+
+
+<h2>The staff at Paradise Physiotherapy is trained to fit your custom hosiery product and decide which would be the best product for you.</h2>
+</p>
---
+++ ourteam.php
@@ -0,0 +1,15 @@
+<?php
+    $title = 'Our Team';
+    require_once 'includes/header.php';
+    require_once 'db/conn.php';
+    $result = $crud->getSpecialties();
+
+?>
+<p class="text-primary font-weight-bold ">
+<h1>Our Multidisciplinary Team at Your Service</h1>
+<h2>REGISTERED PHYSIOTHERAPISTS<h2>
+Wendy Salesman
+
+<h1>ADMINISTRATIVE STAFF</h1>
+<h2>Damian Salesman Office Manager</h2>
+ </p>
